06c9841801035a6961c8f613f7d4bda5dd026376
Parthaasarathy <partha08sarathy@gmail.com>
2026-03-17 00:26:40 +0530

    day 2

---
+++ Int.py
@@ -0,0 +1,11 @@
+x = float(input("Enter the First Number: "))
+y = float(input("Enter the Second Number: "))
+z = x/y
+print(f"{z:.2f}")
+"""The code takes two numbers as input from the user, performs division, 
+and then prints the result formatted to two decimal places.
+Enter the First Number: 2
+Enter the Second Number: 3
+0.67
+"""
+
